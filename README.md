25f028dc1aded64802dcce6e575ce78366cc3e4e
Shirajuki <shirajuki00@gmail.com>
2022-12-09 13:39:33 +0100

    chore: add java day6 challenge solution

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # advent-of-code
 
-My solutions to the Advent of Code 2022, golf style, written in some programming languages.
+My solutions to the Advent of Code 2022, golf style, written in some different programming languages.
 
 - [x] Day 01: Python3
 - [x] Day 02: Javascript
 - [x] Day 03: Java
 - [x] Day 04: Python3
 - [x] Day 05: Javascript
-- [ ] Day 06: ???
+- [x] Day 06: Java
 - [ ] Day 07: ???
 - [ ] Day 08: ???
---
+++ day6/Day6.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.*;
+import java.util.*;
+interface Day6 {
+	static int x(int n,int[]s){return (int)IntStream.range(n,s.length).takeWhile(i->IntStream.range(i-n,i+1).map(j->s[j]).distinct().count()!=n+1).count()+n+1;}
+	static void main(String[]a) throws Exception{
+		int[] s=Files.readString(new File("input").toPath()).chars().toArray();
+		System.out.println(x(3,s)+" "+x(13,s));
+	}
+}
