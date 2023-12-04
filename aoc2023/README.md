1f964f21a5aa0d756afcf0c22cf68a6cbd87c28d
Jonny Ngo Luong <jonny.luong@omegapoint.no>
2023-12-04 17:11:36 +0100

    feat: update file structure, add day1-2 aoc2023

---
+++ aoc2023/README.md
@@ -0,0 +1,5 @@
+# advent-of-code
+
+My solutions to the Advent of Code 2023, golf style on most so that I get to use my puny brain a little.
+
+Solutions are written in different programming languages for learning purposes.
---
+++ aoc2023/day01/day01.py
@@ -0,0 +1,5 @@
+# Part 1
+print(sum([[n:=[b[i] for i in range(len(b)) if b[i].isdigit()],int(n[0]*2 if len(n)==1 else n[0]+n[-1])][1] for b in open('input.txt').read().split('\n')]))
+
+# Part 2
+print(sum([[[c:=['ø','one','two','three','four','five','six','seven','eight','nine'],n:=[],[[i==b.find(d,i) and n.append(str(c.index(d))) or b[i].isdigit() and n.append(b[i]) for d in c] for i in range(len(b))]],int(n[0]*2 if len(n)==1 else n[0]+n[-1])][1] for b in open('input.txt').read().split('\n')]))
